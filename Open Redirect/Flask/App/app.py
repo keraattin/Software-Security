415c01f041b31d531224bee422e17ddd52a36c94
keraattin <kerem-oruc@outlook.com>
2023-10-25 13:42:40 +0300

    Docker support added

---
+++ Open Redirect/Flask/App/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Vulnerability:        Open Redirect
+# Language:             Python
+# Language Version:     3.10
+# Framework:            Flask
+# Framework Version:    2.3.2
+# Enviroment:           Web
+
+
+# Example Request
+# http://localhost:5000/redirect?next=https://malicioussite.xyz
+
+# Libraries
+from flask import Flask,request,redirect
+
+# App
+app = Flask(__name__)
+
+# Routes
+@app.route("/")
+def home():
+      return """
+        <h3>Example Request</h3>
+        <i>http://localhost:5000/redirect?next=https://malicioussite.xyz</i>  
+        """
+
+# Vulnerable Routes
+@app.route("/redirect")
+def redirected_page():
+    next = request.args.get("next")
+    return redirect(next)
+
+# Main
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
